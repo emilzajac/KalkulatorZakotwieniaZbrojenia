28238f67fc223b5cb0a8ab4d43691f49ad4e186d
Emil <emilzajac@gmail.com>
2017-12-03 21:37:06 +0100

    Zmieniono funkcję getKlasa zwaracjącą wytrzymałość betonu w klasie Beton

--- src/com/zakotwieniezbrojenia/model/Beton.java
+++ src/com/zakotwieniezbrojenia/model/Beton.java
@@ -22,10 +22,27 @@ public class Beton {
      * @return Klasa betonu np. 15MPa
      */
     public int getKlasa() {
-        int klasaInt = Integer.valueOf(klasa.substring(1));
-        return klasaInt;
-    }
-    
-    
-    
+        int wytrzymaloscNaSciskanie_fck = 0;
+        
+        if (klasa.equals("B15")) {
+           wytrzymaloscNaSciskanie_fck = 12; 
+        }else if (klasa.equals("B20")) {
+            wytrzymaloscNaSciskanie_fck = 16;
+        }else if (klasa.equals("B25")) {
+            wytrzymaloscNaSciskanie_fck = 20;
+        }else if (klasa.equals("B30")) {
+            wytrzymaloscNaSciskanie_fck = 25;
+        }else if (klasa.equals("B37")) {
+            wytrzymaloscNaSciskanie_fck = 30;
+        }else if (klasa.equals("B45")) {
+            wytrzymaloscNaSciskanie_fck = 35;
+        }else if (klasa.equals("B50")) {
+            wytrzymaloscNaSciskanie_fck = 40;
+        }else if (klasa.equals("B55")) {
+            wytrzymaloscNaSciskanie_fck = 45;
+        }else if (klasa.equals("B60")) {
+            wytrzymaloscNaSciskanie_fck = 50;
+        }
+        return wytrzymaloscNaSciskanie_fck;
+    } 
 }
